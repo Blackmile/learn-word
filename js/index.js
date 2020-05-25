5812b4a6d72a881618bf4b4b9e10ef7ad8a1fa8f
Blackmile <stanleyeze758@gmail.com>
2020-05-25 13:11:36 +0100

    good

---
+++ js/index.js
@@ -0,0 +1,45 @@
+/*var firebaseConfig = {
+    apiKey: "AIzaSyB4rtE6PMqXs2OItW1-uFLE9Ra81tQ0HFM",
+    authDomain: "reformed-b521d.firebaseapp.com",
+    databaseURL: "https://reformed-b521d.firebaseio.com",
+    projectId: "reformed-b521d",
+    storageBucket: "reformed-b521d.appspot.com",
+    messagingSenderId: "1092837700617",
+    appId: "1:1092837700617:web:388e6bbc08a2efc48bf4d8",
+    measurementId: "G-KR13PRXJPW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();*/
+
+  //firebase.auth.Auth.Persistence.SESSION;
+
+
+
+  $("#btn-login").click(function(){
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    if(email != "" && password != ""){
+        var result = firebase.auth().signInWithEmailAndPassword(email, password);
+
+
+        result.catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            window.alert("Message : " + errorMessage);
+        });
+    }
+    else{
+        window.alert("form is incomplete. please fill out all fields.");
+    }
+
+    console.log("New user =" + email + " " + password);
+
+
+
+  });
