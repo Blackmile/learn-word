69cef715bc5dc8a041caa57d1890f57c45a01287
Blackmile <stanleyeze758@gmail.com>
2020-09-30 12:32:51 +0100

    set persistence

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+.then(function() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  // In memory persistence will be applied to the signed in Google user
+  // even though the persistence was set to 'none' and a page redirect
+  // occurred.
+  return firebase.auth().signInWithRedirect(provider);
+})
+.catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
